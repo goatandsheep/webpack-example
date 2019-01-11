56b5bd21f95fa3a5eee528c979c397a8e21ef4e2
Kemal Ahmed <goatandsheep@gmail.com>
2019-01-11 18:43:07 -0500

    changed to fit webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
 	mode: "development",
 	devtool: "sourcemap",
-	entry: "./demo/index.ts",
+	entry: "./demo/index.js",
 	output: {
 		path: __dirname + "/demo/bundle",
 		filename: "index.js",
